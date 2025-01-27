68d93d475976b6113e165d5ece24aef63d1d8a58
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-01-27 23:30:03 +0200

    feat: implement custom drawer and user info list tile components

---
+++ lib/widgets/user_info_list_tile.dart
@@ -0,0 +1,26 @@
+import 'package:dash_board/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class UserInfoListTile extends StatelessWidget {
+  const UserInfoListTile(
+      {super.key,
+      required this.image,
+      required this.title,
+      required this.subTitle});
+  final String image, title, subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(image),
+      title: Text(
+        title,
+        style: AppStyles.styleSemiBold16(context),
+      ),
+      subtitle: Text(
+        subTitle,
+        style: AppStyles.styleRegular16(context),
+      ),
+    );
+  }
+}
