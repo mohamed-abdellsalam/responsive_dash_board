0ba146dc3c8901103510521c0696ffbe00b54553
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-02-11 20:12:14 +0200

    finish cards

---
+++ lib/widgets/my_cards_page_view.dart
@@ -0,0 +1,18 @@
+import 'package:dash_board/widgets/my_card.dart';
+import 'package:expandable_page_view/expandable_page_view.dart';
+import 'package:flutter/material.dart';
+
+class MyCardsPageView extends StatelessWidget {
+  const MyCardsPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandablePageView(
+      scrollDirection: Axis.horizontal,
+      children: List.generate(
+        3,
+        (index) => const MyCard(),
+      ),
+    );
+  }
+}
