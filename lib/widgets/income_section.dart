fe342c0315e780506056e838f97fbe1106ff6653
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-02-13 12:50:17 +0200

    finish income section header

---
+++ lib/widgets/income_section.dart
@@ -0,0 +1,21 @@
+import 'package:dash_board/widgets/custom_background_container.dart';
+import 'package:dash_board/widgets/income_section_header.dart';
+import 'package:flutter/material.dart';
+
+class IncomeSection extends StatelessWidget {
+  const IncomeSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgroundContainer(
+      child: Column(
+        children: [
+          IncomeSectionHeader(),
+          Row(
+            children: [],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/income_section_header.dart
@@ -0,0 +1,54 @@
+import 'package:dash_board/utils/app_styles.dart';
+import 'package:flutter/material.dart';
+
+class IncomeSectionHeader extends StatelessWidget {
+  const IncomeSectionHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Income',
+          style: AppStyles.styleSemiBold20(context),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 12,
+          ),
+          decoration: ShapeDecoration(
+            color: Colors.white,
+            shape: RoundedRectangleBorder(
+              side: const BorderSide(
+                width: 1,
+                color: Color(0xfff1f1f1),
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          child: Row(
+            children: [
+              Text(
+                'Monthly',
+                style: AppStyles.styleMedium16(context),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Transform.rotate(
+                angle: -1.57079633,
+                child: const Icon(
+                  Icons.arrow_back_ios_new,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
