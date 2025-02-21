9bfab5b5f28374a77b350d68c57145039176fb31
Mohamed Abdelsalam <mohamedabdsalam185@gmail.com>
2025-02-21 18:54:00 +0200

    feat: implement responsive font sizing in AppStyles for improved layout adaptability

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -3,90 +3,90 @@ import 'package:flutter/material.dart';
 
 abstract class AppStyles {
   static TextStyle styleRegular16(context) {
-    return const TextStyle(
-      color: Color(0xFF064060),
-      fontSize: 16,
+    return TextStyle(
+      color: const Color(0xFF064060),
+      fontSize: getResponsiveFontSize(fontSize: 16, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w400,
     );
   }
 
   static TextStyle styleBold16(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFF4EB7F2),
-      fontSize: 16,
+    return TextStyle(
+      color: const Color(0xFF4EB7F2),
+      fontSize: getResponsiveFontSize(fontSize: 16, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w700,
     );
   }
 
   static TextStyle styleMedium16(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFF064061),
-      fontSize: 16,
+    return TextStyle(
+      color: const Color(0xFF064061),
+      fontSize: getResponsiveFontSize(fontSize: 16, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w500,
     );
   }
 
   static TextStyle styleMedium20(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFFFFFFFF),
-      fontSize: 20,
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(fontSize: 20, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w500,
     );
   }
 
   static TextStyle styleSemiBold16(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFF064061),
-      fontSize: 16,
+    return TextStyle(
+      color: const Color(0xFF064061),
+      fontSize: getResponsiveFontSize(fontSize: 16, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w600,
     );
   }
 
   static TextStyle styleSemiBold20(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFF064061),
-      fontSize: 20,
+    return TextStyle(
+      color: const Color(0xFF064061),
+      fontSize: getResponsiveFontSize(fontSize: 20, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w600,
     );
   }
 
   static TextStyle styleRegular12(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFFAAAAAA),
-      fontSize: 12,
+    return TextStyle(
+      color: const Color(0xFFAAAAAA),
+      fontSize: getResponsiveFontSize(fontSize: 12, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w400,
     );
   }
 
   static TextStyle styleSemiBold24(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFF4EB7F2),
-      fontSize: 24,
+    return TextStyle(
+      color: const Color(0xFF4EB7F2),
+      fontSize: getResponsiveFontSize(fontSize: 24, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w600,
     );
   }
 
   static TextStyle styleRegular14(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFFAAAAAA),
-      fontSize: 14,
+    return TextStyle(
+      color: const Color(0xFFAAAAAA),
+      fontSize: getResponsiveFontSize(fontSize: 14, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w400,
     );
   }
 
   static TextStyle styleSemiBold18(BuildContext context) {
-    return const TextStyle(
-      color: Color(0xFFFFFFFF),
-      fontSize: 18,
+    return TextStyle(
+      color: const Color(0xFFFFFFFF),
+      fontSize: getResponsiveFontSize(fontSize: 18, context),
       fontFamily: 'Montserrat',
       fontWeight: FontWeight.w600,
     );
